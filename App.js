9f2a7e8323f3faccffbbfea76df8685c8b3b2eaa
dev-nik98 <niharikakanwar333@gmail.com>
2024-04-09 22:18:11 +0530

    platform

--- App.js
+++ App.js
@@ -6,70 +6,28 @@
  */
 
 import React, {useState} from 'react';
-import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
+import {
+  Text,
+  View,
+ 
+  StyleSheet,
+  Platform,
+} from 'react-native';
 
 const App = () => {
-  const [selectedRadio, setSelectedRadio] = useState(1);
-  console.log('------', selectedRadio)
   return (
-    <View style={styles.main}>
-      <TouchableOpacity onPress={()=>{
-        console.log('djkgdjkgbj')
-        setSelectedRadio(1)}}>
-        <View style={styles.radioWraper}>
-          <View style={styles.radio}>
-            {
-            selectedRadio===1 ? <View style={styles.radioBg} ></View>:null
-            }
-          </View>
-          <Text style={styles.radioText}>radio 1</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={()=>setSelectedRadio(2)}>
-        <View style={styles.radioWraper}>
-          <View style={styles.radio}>
-            {
-            selectedRadio===2 ? <View style={styles.radioBg} ></View>:null
-            }
-          </View>
-          <Text style={styles.radioText}>radio 2</Text>
-        </View>
-      </TouchableOpacity>
+    <View>
+      <Text style={{fontSize: 30}}>Platform:{Platform.OS}</Text>
+      {
+      Platform.OS ? 
+        <View style={{backgroundColor:'pink', height:100, width:100}}/>
+       : 
+        <View style={{backgroundColor:'blue', height:100, width:100}}/>
+      }
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  radioText: {
-    fontSize: 20,
-    color: 'red',
-  },
-  radio: {
-    height: 40,
-    width: 40,
-    borderColor: 'red',
-    borderWidth: 2,
-    borderRadius: 20,
-    margin: 10,
-    
-  },
-  
-  radioBg: {
-    backgroundColor: 'green',
-    height: 28,
-    width: 28,
-    borderRadius: 20,
-    margin: 4,
-
-  },
-  radioWraper: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  }
-});
+const styles = StyleSheet.create({});
 
 export default App;
